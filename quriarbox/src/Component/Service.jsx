96ae66d57370ee58a4bc56fbfb8388975f50fda1
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-12 03:31:15 +0600

    service box done

--- quriarbox/src/Component/Service.jsx
+++ quriarbox/src/Component/Service.jsx
@@ -12,15 +12,93 @@ const Service = () => {
       </div>
       {/* text */}
 
-      <div className='flex  py-20'>
-        <div className=' bg-white py-40 px-24'>
+      <div className='flex  py-20 justify-between gap-4'>
+        <div className=' bg-white py-40 px-24 w-[424px]'>
             <img className='mx-auto' src={icon}/>
             <h1 className='mt-7 mx-auto text-center font-ral text-2xl font-extrabold text-[#464558] w-[222px] h-[26px]'>Business Services</h1>
             <p className='mt-8 font-ral text-base font-normal text-[#7B7A8B] w-[310px]  mx-auto '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
-            
+           <div className='mt-4'>
+           <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Corporate goods</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Shipment</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Accesories</p>
+          
+          </div>
+          <div className='mt-14 flex justify-center'>
+            <button className='py-3 px-5 w-[301px] bg-comon text-xl text-white font-bold rounded-md  '>Get started</button>
         </div>
-        <div className=''></div>
-        <div className=''></div>
+           </div>
+        </div>
+
+        <div className=' bg-white py-40 px-24 w-[424px]'>
+            <img className='mx-auto' src={icon}/>
+            <h1 className='mt-7 mx-auto text-center font-ral text-2xl font-extrabold text-[#464558] w-[222px] h-[26px]'>Personal Services</h1>
+            <p className='mt-8 font-ral text-base font-normal text-[#7B7A8B] w-[310px]  mx-auto '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+           <div className='mt-4'>
+           <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Corporate goods</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Shipment</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Accesories</p>
+          
+          </div>
+          <div className='mt-14 flex justify-center'>
+            <button className='py-3 px-5 w-[301px] text-xl text-comon border-2 border-comon font-bold rounded-md  '>Get started</button>
+        </div>
+           </div>
+        </div>
+
+        <div className=' bg-white py-40 px-24 w-[424px]'>
+            <img className='mx-auto' src={icon}/>
+            <h1 className='mt-7 mx-auto text-center font-ral text-2xl font-extrabold text-[#464558] w-[230px] h-[26px]'>Statewide Services</h1>
+            <p className='mt-8 font-ral text-base font-normal text-[#7B7A8B] w-[310px]  mx-auto '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+           <div className='mt-4'>
+           <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Corporate goods</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Shipment</p>
+          
+          </div>
+          <div className='flex gap-4 mt-3'>
+          
+          <a className='w-[7.31px] h-[7px] bg-comon rounded-full mt-[9px]'> </a>  
+          <p className='font-ral text-[#7B7A8B] text-base font-normal text-center'>Accesories</p>
+          
+          </div>
+          <div className='mt-14 flex justify-center'>
+            <button className='py-3 px-5 w-[301px] text-xl text-comon border-2 border-comon font-bold rounded-md  '>Get started</button>
+        </div>
+           </div>
+        </div>
       </div>
     </div>
 
